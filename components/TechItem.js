31bc833ff9f2697cc2a27b036aa85765c647c3e0
GangbacOl <gangyeonjo336@gmail.com>
2021-03-11 23:15:28 +0900

    Update: update progress bar design, add box shadow

--- components/TechItem.js
+++ components/TechItem.js
@@ -5,7 +5,9 @@ export default function TechItem({ name, description, percentage }) {
         <Container>
             <Name>{name}</Name>
             <Description>{description}</Description>
-            <span>{percentage}</span>
+            <Progress>
+                <ProgressBar percentage={percentage} />
+            </Progress>
         </Container>
     );
 }
@@ -13,7 +15,30 @@ export default function TechItem({ name, description, percentage }) {
 const Container = styled.div`
     width: 100%;
     max-width: 650px;
-    border: 1px solid black;
+    background: #f7f7f7;
+    -webkit-box-shadow: 1px 1px 10px -4px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 1px 1px 10px -4px rgba(0, 0, 0, 0.75);
+    box-shadow: 1px 1px 10px -4px rgba(0, 0, 0, 0.75);
+    border-radius: 5px;
+    padding: 20px;
 `;
-const Name = styled.h2``;
-const Description = styled.p``;
+
+const Name = styled.h2`
+    margin: 0 0 10px 0;
+`;
+
+const Description = styled.p`
+    margin: 0 0 10px 0;
+`;
+
+const Progress = styled.div`
+    width: 100%;
+    border: 2px solid black;
+    height: 10px;
+`;
+
+const ProgressBar = styled.div`
+    width: ${(props) => props.percentage}%;
+    height: 6px;
+    background: #000;
+`;
