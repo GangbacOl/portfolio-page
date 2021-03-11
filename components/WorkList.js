26f2904155fbec53f254834614b578936ef28924
GangbacOl <gangyeonjo336@gmail.com>
2021-03-11 22:07:49 +0900

    Update: split work data, jsx, components design

--- components/WorkList.js
+++ components/WorkList.js
@@ -3,47 +3,111 @@ import Image from 'next/image';
 
 import WorkItem from './WorkItem';
 
-const workData = [
+const personalWork = [
     {
         name: '유사 자율주행 rc카',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, nesciunt! Itaque illum debitis officiis magni veritatis deserunt in expedita optio nulla tenetur aliquam, voluptate doloribus eius quae impedit laudantium corrupti!',
+        imagePath: '/images/rccar.jpg',
     },
+];
+const teamWork = [
     {
         name: '정보과 홍보 페이지',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, nesciunt! Itaque illum debitis officiis magni veritatis deserunt in expedita optio nulla tenetur aliquam, voluptate doloribus eius quae impedit laudantium corrupti!',
+        imagePath: '/images/sunrinsecurity.png',
     },
     {
         name: 'STA+C TranSign',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, nesciunt! Itaque illum debitis officiis magni veritatis deserunt in expedita optio nulla tenetur aliquam, voluptate doloribus eius quae impedit laudantium corrupti!',
+        imagePath: '/images/transign.png',
     },
+];
+
+const competitionWork = [
     {
-        name: '[Intel] Intel AI For Youth',
+        name: 'Intel AI For Youth',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, nesciunt! Itaque illum debitis officiis magni veritatis deserunt in expedita optio nulla tenetur aliquam, voluptate doloribus eius quae impedit laudantium corrupti!',
+        imagePath: '/images/intel.jpg',
+        type: 'competition',
     },
     {
-        name: '[KAIST 양재R&D혁신허브] 2019 혁신허브 AI스쿨 입문자 양성과정 2기',
+        name: '2020 데이터 크리에이터 캠프',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, nesciunt! Itaque illum debitis officiis magni veritatis deserunt in expedita optio nulla tenetur aliquam, voluptate doloribus eius quae impedit laudantium corrupti!',
+        imagePath: '/images/datacreator.jpg',
+        type: 'competition',
     },
+];
+
+const educationWork = [
     {
-        name: '[한국정보화진흥원] 2020 데이터 크리에이터 캠프',
+        name: '2019 혁신허브 AI스쿨 입문자 양성과정 2기',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, nesciunt! Itaque illum debitis officiis magni veritatis deserunt in expedita optio nulla tenetur aliquam, voluptate doloribus eius quae impedit laudantium corrupti!',
+        imagePath: '/images/yangjae.jpg',
+        type: 'education',
     },
 ];
 
 export default function WorkList() {
     return (
         <Container>
-            <WorkItemWrap>
-                {workData.map((item, idx) => (
-                    <WorkItem name={item.name} key={idx} />
-                ))}
-            </WorkItemWrap>
+            <WorkItemsWrap>
+                <Title>개인 프로젝트{' (' + personalWork.length + ')'}</Title>
+                <WorkItems>
+                    {personalWork.map((item, idx) => (
+                        <WorkItem
+                            name={item.name}
+                            description={item.description}
+                            imagePath={item.imagePath}
+                            key={idx}
+                        />
+                    ))}
+                </WorkItems>
+            </WorkItemsWrap>
+            <WorkItemsWrap>
+                <Title>팀 프로젝트{' (' + teamWork.length + ')'}</Title>
+                <WorkItems>
+                    {teamWork.map((item, idx) => (
+                        <WorkItem
+                            name={item.name}
+                            description={item.description}
+                            imagePath={item.imagePath}
+                            key={idx}
+                        />
+                    ))}
+                </WorkItems>
+            </WorkItemsWrap>
+            <WorkItemsWrap>
+                <Title>대회{' (' + competitionWork.length + ')'}</Title>
+                <WorkItems>
+                    {competitionWork.map((item, idx) => (
+                        <WorkItem
+                            name={item.name}
+                            description={item.description}
+                            imagePath={item.imagePath}
+                            key={idx}
+                        />
+                    ))}
+                </WorkItems>
+            </WorkItemsWrap>
+            <WorkItemsWrap>
+                <Title>교육 & 캠프{' (' + educationWork.length + ')'}</Title>
+                <WorkItems>
+                    {educationWork.map((item, idx) => (
+                        <WorkItem
+                            name={item.name}
+                            description={item.description}
+                            imagePath={item.imagePath}
+                            key={idx}
+                        />
+                    ))}
+                </WorkItems>
+            </WorkItemsWrap>
         </Container>
     );
 }
@@ -51,15 +115,23 @@ export default function WorkList() {
 const Container = styled.div`
     width: 100%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
 `;
-const WorkItemWrap = styled.div`
-    width: 80%;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    justify-items: center;
-    row-gap: 20px;
-    column-gap: 20px;
-    border: 1px solid black;
-    margin: 0 auto;
+
+const WorkItemsWrap = styled.div`
+    width: 75%;
+`;
+
+const WorkItems = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const Title = styled.h2`
+    font-size: 2rem;
+    font-weight: 500;
+    border-bottom: 3px solid black;
+    padding: 20px;
 `;
