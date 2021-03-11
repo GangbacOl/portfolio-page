1990cdc976799aad0b4634f25deef3c6f4d12e28
GangbacOl <gangyeonjo336@gmail.com>
2021-03-11 22:09:01 +0900

    Update: add title, paragraph tag, component design

--- components/WorkItem.js
+++ components/WorkItem.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 import Image from 'next/image';
 
-export default function WorkItem({ name }) {
-    return <Container>{name}</Container>;
+export default function WorkItem({ name, description, imagePath }) {
+    return (
+        <Container imagePath={imagePath}>
+            <Introduce>
+                <Title>{name}</Title>
+                <Paragraph>{description}</Paragraph>
+            </Introduce>
+        </Container>
+    );
 }
 
 const Container = styled.div`
@@ -11,5 +18,43 @@ const Container = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    border: 1px solid black;
+    background: none;
+    background-image: url(${(props) => props.imagePath});
+    background-size: cover;
+    background-position: center;
+    margin: 20px;
+    & > div {
+        background: none;
+        & h2,
+        & p {
+            opacity: 0;
+        }
+    }
+    &:hover {
+        & > div {
+            background: rgba(0, 0, 0, 0.7);
+            & h2,
+            & p {
+                opacity: 1;
+            }
+        }
+    }
+`;
+
+const Introduce = styled.div`
+    width: 100%;
+    height: 100%;
+    color: white;
+    transition: all 0.3s;
+    padding: 20px;
+`;
+const Title = styled.h2`
+    transition: all 0.2s;
+    font-size: 1.2rem;
+    margin: 10px 0 20px 0;
+`;
+const Paragraph = styled.p`
+    transition: all 0.2s;
+    font-size: 0.9rem;
+    margin: 0;
 `;
