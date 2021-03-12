3372e068d4878b480af34ff7ea7c8a7f49ff8afe
GangbacOl <gangyeonjo336@gmail.com>
2021-03-12 11:48:13 +0900

    Update: update import method

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import { GlobalStyle } from '../styles/globalStyle';
+import { Helmet } from 'react-helmet';
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>GangbacOl Portfolio</title>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+            </Helmet>
             <GlobalStyle />
             <Component {...pageProps} />
         </>
