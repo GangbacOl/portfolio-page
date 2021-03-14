b8f8957aeacc1dc4872bc2b6614850304a45c8a7
GangbacOl <gangyeonjo336@gmail.com>
2021-03-15 00:44:24 +0900

    Update: add imagePath property, style logic

--- components/TechList.js
+++ components/TechList.js
@@ -8,66 +8,77 @@ const TechSkillData = [
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 70,
+        imagePath: '/images/logo/js.png',
     },
     {
         name: 'TypeScript',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 40,
+        imagePath: '/images/logo/ts.png',
     },
     {
         name: 'Python',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 50,
+        imagePath: '/images/logo/python.png',
     },
     {
         name: 'HTML5',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 80,
+        imagePath: '/images/logo/html.png',
     },
     {
         name: 'CSS3',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 80,
+        imagePath: '/images/logo/css.png',
     },
     {
         name: 'Github & Git',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 40,
+        imagePath: '/images/logo/git.png',
     },
     {
         name: 'React.js',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 70,
+        imagePath: '/images/logo/react.png',
     },
     {
         name: 'Next.js',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 60,
+        imagePath: '/images/logo/next.png',
     },
     {
         name: 'Redux.js',
         description:
             'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut nesciunt non commodi cumque. Cum, ratione modi maiores atque impedit omnis distinctio minus a molestiae, pariatur enim odio ab est animi?',
         percentage: 70,
+        imagePath: '/images/logo/redux.png',
     },
 ];
 
 export default function TechList() {
     return (
         <Container>
+            <Title>기술 스택</Title>
             <TechItemsWrap>
                 {TechSkillData.map((item, idx) => (
                     <TechItem
                         name={item.name}
                         description={item.description}
                         percentage={item.percentage}
+                        imagePath={item.imagePath}
                         key={idx}
                     />
                 ))}
@@ -77,11 +88,19 @@ export default function TechList() {
 }
 
 const Container = styled.div`
-    width: 100%;
+    width: 90%;
+    margin: 0 auto;
+`;
+
+const Title = styled.h2`
+    font-size: 2.5rem;
+    font-weight: 500;
+    border-bottom: 3px solid black;
+    padding: 10px 20px;
 `;
 
 const TechItemsWrap = styled.div`
-    width: 90%;
+    width: 100%;
     display: grid;
     grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
     justify-items: center;
