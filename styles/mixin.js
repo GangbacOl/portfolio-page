4c704aebb8a88662c439e7af81be6ca05689a031
GangbacOl <gangyeonjo336@gmail.com>
2021-03-17 00:45:56 +0900

    Add: media query mixin(mobile, tablet)

---
+++ styles/mixin.js
@@ -0,0 +1,17 @@
+const breakpointMobile = 425;
+const breakpointTablet = 768;
+
+export const mobileTablet = (content) =>
+    `@media only screen and (max-width:${breakpointTablet}px){
+        ${content}
+    }`;
+
+export const mobile = (content) =>
+    `@media only screen and (max-width: ${breakpointMobile}px) {
+        ${content}
+    }`;
+
+export const tablet = (content) =>
+    `@media only screen and (min-width: ${breakpointMobile + 1}px) and (max-width:${breakpointTablet}px) {
+        ${content}
+    }`;
