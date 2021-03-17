32004d4d680465c2ad46b00b530d0507cf1b4071
GangbacOl <gangyeonjo336@gmail.com>
2021-03-17 11:09:42 +0900

    Update: set favicon

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,10 @@ function MyApp({ Component, pageProps }) {
                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
                     rel="stylesheet"
                 />
+                <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </Helmet>
             <GlobalStyle />
             <Component {...pageProps} />
