44ea39edf031b5c797ae2368ba950fd42049d639
GangbacOl <gangyeonjo336@gmail.com>
2021-03-17 18:31:47 +0900

    Update: add open graph

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ function MyApp({ Component, pageProps }) {
         <>
             <Helmet>
                 <meta charSet="utf-8" />
+                <meta property="og:title" content="GangbacOl 포트폴리오 페이지" />
+                <meta
+                    property="og:description"
+                    content="안녕하세요, 웹 프론트엔드를 공부하는 GangbacOl입니다."
+                />
                 <title>GangbacOl Portfolio</title>
                 <link
                     href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
