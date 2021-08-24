a820373f00b2a802849e43668881a4eff2e0a9d1
GangbacOl <gangyeonjo336@gmail.com>
2021-08-24 10:48:52 +0900

    Update: nav item text 'TechList' to 'Skills'

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,60 +1,60 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import * as mixin from '../styles/mixin';
+import * as mixin from "../styles/mixin";
 
 export default function MainNav({ about, works, tech }) {
-    const handleScroll = (element) => {
-        if (element) window.scrollTo({ top: element.current.offsetTop, left: 0, behavior: 'smooth' });
-    };
-    return (
-        <Container>
-            <Navigation>
-                <NavList onClick={() => handleScroll(about)}>About</NavList>
-                <NavList onClick={() => handleScroll(works)}>Works</NavList>
-                <NavList onClick={() => handleScroll(tech)}>Tech Stack</NavList>
-            </Navigation>
-        </Container>
-    );
+  const handleScroll = (element) => {
+    if (element) window.scrollTo({ top: element.current.offsetTop, left: 0, behavior: "smooth" });
+  };
+  return (
+    <Container>
+      <Navigation>
+        <NavList onClick={() => handleScroll(about)}>About</NavList>
+        <NavList onClick={() => handleScroll(works)}>Works</NavList>
+        <NavList onClick={() => handleScroll(tech)}>Skills</NavList>
+      </Navigation>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    width: 100%;
-    margin: 200px 0 100px 0;
-    ${mixin.mobileTablet(`
+  width: 100%;
+  margin: 200px 0 100px 0;
+  ${mixin.mobileTablet(`
         margin: 0 0 100px 0;
     `)}
 `;
 const Navigation = styled.div`
-    width: 60%;
-    max-width: 950px;
-    height: 60px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    list-style: none;
-    margin: 0 auto;
-    border: 4px solid #eee;
-    border-radius: 20px;
-    & li:first-child {
-        border-top-left-radius: 15px;
-        border-bottom-left-radius: 15px;
-    }
-    & li:last-child {
-        border-top-right-radius: 15px;
-        border-bottom-right-radius: 15px;
-    }
+  width: 60%;
+  max-width: 950px;
+  height: 60px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  margin: 0 auto;
+  border: 4px solid #eee;
+  border-radius: 20px;
+  & li:first-child {
+    border-top-left-radius: 15px;
+    border-bottom-left-radius: 15px;
+  }
+  & li:last-child {
+    border-top-right-radius: 15px;
+    border-bottom-right-radius: 15px;
+  }
 `;
 const NavList = styled.li`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    transition: all 0.3s;
-    font-weight: bold;
-    color: #eee;
-    &:hover {
-        background-color: #eee;
-        color: #000;
-    }
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  transition: all 0.3s;
+  font-weight: bold;
+  color: #eee;
+  &:hover {
+    background-color: #eee;
+    color: #000;
+  }
 `;
--- components/TechList.js
+++ components/TechList.js
@@ -1,117 +1,112 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import TechItem from './TechItem';
+import TechItem from "./TechItem";
 
-import { mobile } from '../styles/mixin';
+import { mobile } from "../styles/mixin";
 
 export default function TechList() {
-    return (
-        <Container>
-            <Title>Tech Stack</Title>
-            <TechItemsWrap>
-                {TechSkillData.map((item, idx) => (
-                    <TechItem
-                        name={item.name}
-                        percentage={item.percentage}
-                        imagePath={item.imagePath}
-                        key={idx}
-                    />
-                ))}
-            </TechItemsWrap>
-        </Container>
-    );
+  return (
+    <Container>
+      <Title>Skills</Title>
+      <TechItemsWrap>
+        {TechSkillData.map((item, idx) => (
+          <TechItem name={item.name} percentage={item.percentage} imagePath={item.imagePath} key={idx} />
+        ))}
+      </TechItemsWrap>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    width: 95%;
-    margin: 0 auto;
+  width: 95%;
+  margin: 0 auto;
 `;
 
 const Title = styled.h2`
-    font-size: 2.5rem;
-    font-weight: 500;
-    padding: 0px 20px 10px 20px;
-    border-bottom: 2px solid black;
-    margin-top: 0;
+  font-size: 2.5rem;
+  font-weight: 500;
+  padding: 0px 20px 10px 20px;
+  border-bottom: 2px solid black;
+  margin-top: 0;
 `;
 
 const TechItemsWrap = styled.div`
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
-    justify-items: center;
-    row-gap: 20px;
-    column-gap: 20px;
-    margin: 0 auto;
-    ${mobile(`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+  justify-items: center;
+  row-gap: 20px;
+  column-gap: 20px;
+  margin: 0 auto;
+  ${mobile(`
         grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
     `)}
 `;
 
 const TechSkillData = [
-    {
-        name: 'JavaScript',
-        percentage: 70,
-        imagePath: '/images/logo/js.png',
-    },
-    {
-        name: 'TypeScript',
-        percentage: 40,
-        imagePath: '/images/logo/ts.png',
-    },
-    {
-        name: 'Python',
-        percentage: 50,
-        imagePath: '/images/logo/python.png',
-    },
-    {
-        name: 'HTML5',
-        percentage: 80,
-        imagePath: '/images/logo/html.png',
-    },
-    {
-        name: 'CSS3',
-        percentage: 80,
-        imagePath: '/images/logo/css.png',
-    },
-    {
-        name: 'Github & Git',
-        percentage: 40,
-        imagePath: '/images/logo/git.png',
-    },
-    {
-        name: 'React.js',
-        percentage: 70,
-        imagePath: '/images/logo/react.png',
-    },
-    {
-        name: 'Next.js',
-        percentage: 60,
-        imagePath: '/images/logo/next.png',
-    },
-    {
-        name: 'Redux.js',
-        percentage: 70,
-        imagePath: '/images/logo/redux.png',
-    },
-    {
-        name: 'Sass',
-        percentage: 65,
-        imagePath: '/images/logo/sass.png',
-    },
-    {
-        name: 'styled-components',
-        percentage: 80,
-        imagePath: '/images/logo/styled.png',
-    },
-    {
-        name: 'Amazon Web Service',
-        percentage: 25,
-        imagePath: '/images/logo/aws.png',
-    },
-    {
-        name: 'React Hooks',
-        percentage: 60,
-        imagePath: '/images/logo/hooks.png',
-    },
+  {
+    name: "JavaScript",
+    percentage: 70,
+    imagePath: "/images/logo/js.png",
+  },
+  {
+    name: "TypeScript",
+    percentage: 40,
+    imagePath: "/images/logo/ts.png",
+  },
+  {
+    name: "Python",
+    percentage: 50,
+    imagePath: "/images/logo/python.png",
+  },
+  {
+    name: "HTML5",
+    percentage: 80,
+    imagePath: "/images/logo/html.png",
+  },
+  {
+    name: "CSS3",
+    percentage: 80,
+    imagePath: "/images/logo/css.png",
+  },
+  {
+    name: "Github & Git",
+    percentage: 40,
+    imagePath: "/images/logo/git.png",
+  },
+  {
+    name: "React.js",
+    percentage: 70,
+    imagePath: "/images/logo/react.png",
+  },
+  {
+    name: "Next.js",
+    percentage: 60,
+    imagePath: "/images/logo/next.png",
+  },
+  {
+    name: "Redux.js",
+    percentage: 70,
+    imagePath: "/images/logo/redux.png",
+  },
+  {
+    name: "Sass",
+    percentage: 65,
+    imagePath: "/images/logo/sass.png",
+  },
+  {
+    name: "styled-components",
+    percentage: 80,
+    imagePath: "/images/logo/styled.png",
+  },
+  {
+    name: "Amazon Web Service",
+    percentage: 25,
+    imagePath: "/images/logo/aws.png",
+  },
+  {
+    name: "React Hooks",
+    percentage: 60,
+    imagePath: "/images/logo/hooks.png",
+  },
 ];
